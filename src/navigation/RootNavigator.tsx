4fb7ea15b576501a092e039071b1c60aff7d19f8
dharam-rudra2022 <dharamveer@rudrainnovative.com>
2023-01-03 18:39:42 +0530

    first commit

---
+++ src/navigation/RootNavigator.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import HomeScreen from '../screens/HomeScreen';
+import DetailsScreen from '../screens/HomeScreen';
+
+import { ActivityIndicator } from 'react-native';
+
+const linking = {
+    prefixes: ['deeplinkapp://'],
+    config:{
+        initialRouteName:'Home',
+        screens:{
+            Home: {
+                path:'home'
+            },
+            Details: {
+                path:'details'
+            }
+        }
+    }
+};
+
+const RootStack = createNativeStackNavigator();
+
+const RootNavigator = () => {
+    return(
+        <NavigationContainer
+        linking={linking}
+        fallback={<ActivityIndicator color="blue" size="large"/>}
+        >
+            <RootStack.Navigator>
+                <RootStack.Screen name="Home" component={HomeScreen} />
+                <RootStack.Screen name="Details" component={DetailsScreen} />
+            </RootStack.Navigator>
+        </NavigationContainer>
+    );
+};
+
+export default RootNavigator;
